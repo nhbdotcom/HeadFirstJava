eeb3617b4c943d1a734e84c78ea2540868fc6a3b
nhb.com <nhb_jr@yahoo.com>
2024-09-21 11:16:37 +0800

    Add Doobee proj

---
+++ SharpenYourPencil/Doobee/Doobee.java/src/Doobee.java
@@ -0,0 +1,14 @@
+public class Doobee {
+    public static void main(String[] args){
+        int x = 0;
+
+        while (x < 2) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x++;
+        } 
+        if (x == 2) {
+            System.out.print("Do\n");
+        }
+    }
+}
