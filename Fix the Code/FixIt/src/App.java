25061af09e61011fe4ba82b2a550eed1bb23202c
nhb.com <nhb_jr@yahoo.com>
2024-09-24 23:18:48 +0800

    Added increment so there will be output when running the code

---
+++ Fix the Code/FixIt/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 5) {
+            if (x < 3) {
+                System.out.println("Big x");
+            } x++;
+        }
+    }
+}
