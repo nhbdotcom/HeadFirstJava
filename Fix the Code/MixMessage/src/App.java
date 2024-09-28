f6d754dda59e1e8b3942f7cf77cc779f1ee4ed7e
nhb.com <nhb_jr@yahoo.com>
2024-09-28 23:10:47 +0800

    Done matching the answers

--- Fix the Code/MixMessage/src/App.java
+++ Fix the Code/MixMessage/src/App.java
@@ -3,26 +3,24 @@ public class App {
     int x = 0;
     int y = 0;
     while ( x < 5 ) {
-        //Missing code here
-        System.out.print(x + "" + y + "");
+        //y = x - y; // output 00 11 21 32 42
+        //y = y + x; // output 00 11 23 36 410
+        /*y = y + 2;
+        if( y > 4 ) {
+            y = y - 1;
+        } // output 02 14 25 36 47*/
+        /*x = x + 1;
+        y = y + x;// output 11 34 59*/
+        /*if ( y < 5 ) {
+            x = x + 1;
+            if ( y < 3 ) {
+            x = x - 1;
+            }
+        }
+        y = y + 2;// output 02 14 36 48*/
+        System.out.print(x + "" + y + " ");
         x = x + 1;
         }
+        System.out.println();
     }
 }
-/*Candidates:				Possible output:
-y = x - y;                  22 46
-y = y + x;                  11 34 59
-y = y + 2;                  02 14 26 38
-if( y > 4 ) {               02 14 36 48
-y = y - 1;
-}               
-x = x + 1;                  00 11 21 32 42
-y = y + x;                  11 21 32 42 53
-
-if ( y < 5 ) {              ```00 11 23 36 410```
-    x = x + 1;
-    if ( y < 3 ) {          ```02 14 25 36 47```
-    x = x - 1;
-    }
-}
-y = y + 2;*/
