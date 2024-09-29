eca78c9e20c52a4ff39c5d41da8d456083e1d80c
nhb.com <nhb_jr@yahoo.com>
2024-09-29 23:14:26 +0800

    Pool puzzle one template and choices

---
+++ Fix the Code/Pool Puzzle/src/PoolPuzzleOne.java
@@ -0,0 +1,47 @@
+public class PoolPuzzleOne {
+    public static void main(String[] args) throws Exception {
+    //     while (_______) {
+    //         ____________
+    //         if(x < 1){
+    //             _________
+    //         } _________
+    //         if(_____){
+
+    //         }
+    //         if (x == 1) {
+    //             __________
+    //         }
+    //         if (_____) {
+    //             _____________
+    //         }
+    //         System.out.println("");
+    //         _______________
+    //     }
+    // }
+}
+
+//Note: Each snippet in the pool can only be used once!
+
+/*
+System.out.print(“ ”);
+System.out.print(“a“);
+System.out.print(“n“);
+System.out.print(“an“);
+ 
+x>0
+x<1
+x>1
+x>3
+x<4
+
+x = x + 1;
+x = x + 2;
+x = x - 2;
+x = x - 1;
+
+System.out.print(“noys “);
+System.out.print(“oise “);
+System.out.print(“ oyster “);
+System.out.print(“annoys”);
+System.out.print(“noise”);
+*/
